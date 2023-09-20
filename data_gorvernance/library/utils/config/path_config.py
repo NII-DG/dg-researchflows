f985dcc8370f4a09a7d40be9c93ae2da451e3b0b
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-09-20 14:03:43 +0900

    fix path config

--- data_gorvernance/library/utils/config/path_config.py
+++ data_gorvernance/library/utils/config/path_config.py
@@ -5,15 +5,15 @@
 import os
 from typing import List
 # Folder
-## 同期対象外フォルダ
-DOT_DATA_GOVERNANCE = '.data_gorvernance'
-## 同期対象フォルダ
+### 1階層目
 DATA_GOVERNANCE = 'data_gorvernance'
+### 2階層目
 BASE = 'base'
 SUB_FLOW = 'subflow'
 TASK = 'task'
 RESEARCHFLOW = 'researchflow'
 PLAN = 'plan'
+WORKING = 'working' ## 同期非対称フォルダ
 
 
 # Folder Path
@@ -22,27 +22,14 @@ def get_abs_root_form_working_dg_file_path(working_dg_file_path:str)->str:
     abs_root = working_dg_file_path[0:working_dg_file_path.rfind(DATA_GOVERNANCE)-1]
     return abs_root
 
-def get_dg_sub_flow_base_data_folder()->str:
-    """サブフローベースデータのフォルダパス"""
-    return os.path.join(DATA_GOVERNANCE, BASE, SUB_FLOW)
-def get_dg_task_base_data_folder()->str:
-    """タスクベースデータのフォルダパス"""
-    return os.path.join(DATA_GOVERNANCE, BASE, TASK)
+# サブフローベースデータのフォルダパス
+DG_SUB_FLOW_BASE_DATA_FOLDER = os.path.join(DATA_GOVERNANCE, BASE, SUB_FLOW)
 
-def get_dg_researchflow_folder(is_dot:bool=False)->str:
-    """リサーチフローのフォルダパス
+# タスクベースデータのフォルダパス
+DG_TASK_BASE_DATA_FOLDER = os.path.join(DATA_GOVERNANCE, BASE, TASK)
 
-    Args:
-        is_dot (bool, optional): [真の場合、.data_gorvernance、それ以外はdata_gorvernanceが先頭につく]. Defaults to False.
-
-    Returns:
-        str: [リサーチフローのフォルダパス]
-    """
-
-    if is_dot:
-        return os.path.join(DOT_DATA_GOVERNANCE, RESEARCHFLOW)
-    else:
-        return os.path.join(DATA_GOVERNANCE, RESEARCHFLOW)
+# リサーチフローのフォルダパス
+DG_RESEARCHFLOW_FOLDER = os.path.join(DATA_GOVERNANCE, RESEARCHFLOW)
 
 
 # File
@@ -54,18 +41,13 @@ PLAN_JSON = 'plan.json'
 FLOW_DIAG = 'flow.diag'
 
 # File Path
-SETUP_COMPLETED_TEXT_PATH = os.path.join(DOT_DATA_GOVERNANCE, 'setup_completed.txt')
-PLAN_TASK_STATUS_FILE_PATH = os.path.join(get_dg_researchflow_folder(False), PLAN, STATUS_JSON)
-PLAN_FILE_PATH = os.path.join(get_dg_researchflow_folder(False), PLAN, PLAN_JSON)
+SETUP_COMPLETED_TEXT_PATH = os.path.join(DATA_GOVERNANCE, WORKING, 'setup_completed.txt') # 月岡
+PLAN_TASK_STATUS_FILE_PATH = os.path.join(DG_RESEARCHFLOW_FOLDER, PLAN, STATUS_JSON)
+PLAN_FILE_PATH = os.path.join(DG_RESEARCHFLOW_FOLDER, PLAN, PLAN_JSON)
 
 def get_research_flow_status_file_path(abs_root)->str:
-    return os.path.join(abs_root, get_dg_researchflow_folder(), 'research_flow_status.json')
+    return os.path.join(abs_root, DG_RESEARCHFLOW_FOLDER, 'research_flow_status.json')
 
-def get_abs_root_form_working_dg_file_path(working_dg_file_path:str)->str:
-    if DOT_DATA_GOVERNANCE in working_dg_file_path:
-        return working_dg_file_path[0:working_dg_file_path.rfind(DOT_DATA_GOVERNANCE)-1]
-    else:
-        return working_dg_file_path[0:working_dg_file_path.rfind(DATA_GOVERNANCE)-1]
 
 # other method
 def get_prepare_file_name_list_for_subflow()->List[str]:

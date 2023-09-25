05d0dae21b93d9da56fda61bdc159070d697cfeb
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-09-25 13:45:59 +0900

    setup

--- data_gorvernance/library/main_menu/setup.py
+++ data_gorvernance/library/main_menu/setup.py
@@ -1,8 +1,13 @@
 import os
-from ..utils.config import path_config
+from ..utils.config import path_config, message as msg_config
 from ..subflow.menu import access_main_menu
 from pathlib import Path
 import panel as pn
+from IPython.display import display
+from IPython.core.display import Javascript, HTML
+
+DEFAULT_WIDTH = 600
+SELECT_DEFAULT_VALUE = '--'
 
 SETUP_COMPLETED_FILE = 'setup_completed.txt'
 class ContainerSetter():
@@ -27,24 +32,72 @@ class ContainerSetter():
             os.remove(self.setup_completed_file_path)
         else:
             pass
+    def display_main_menu(self,nb_working_file_path:str):
+        display()
+        access_main_menu(nb_working_file_path)
+
+    def define_setup_form(self):
+        # ユーザ名
+        self.user_name_form = pn.widgets.TextInput(name=msg_config.get('user_auth','username_title'), placeholder=msg_config.get('user_auth','username_help'), width=DEFAULT_WIDTH)
+        # パスワード
+        self.password_form = pn.widgets.PasswordInput(name=msg_config.get('user_auth','password_title'), placeholder=msg_config.get('user_auth','password_help'), width=DEFAULT_WIDTH)
+        # 送信ボタン
+        self.submit_button = pn.widgets.Button(name=msg_config.get('user_auth','end_input'), button_type= "default", width=DEFAULT_WIDTH)
+
+        # システムエラーメッセージオブジェクトの定義
+        self._err_output = pn.WidgetBox()
+        self._err_output.width = 900
+
+
+
+
+
 
     @classmethod
     def setup_form(cls,nb_working_file_path:str):
         pn.extension()
         cs = ContainerSetter(nb_working_file_path)
-        # 初期セットアップが再実行されて場合に備え、初期セットアップ完了ステータスファイルを削除する
-        cs.del_setup_completed_file()
 
-        pass
+        # 初期セットアップ完了ステータスファイルの存在をチェックする
+        if os.path.exists(cs.setup_completed_file_path):
+            # 存在する場合
+            # 初期セットアップ済みとして、メインメニューへのボタンを表示する。
+            alert = pn.pane.Alert(msg_config.get('setup', 'setup_completed'),sizing_mode="stretch_width",alert_type='warning')
+            display(alert)
+            cs.display_main_menu(nb_working_file_path)
+        else:
+            # 存在しない場合
+            # 初期セットアップフォームを表示する。
+
+            ## フォームを定義する
+            cs.define_setup_form()
+            ## フォームを表示する。
+
+            pass
+
 
     @classmethod
     def completed_setup(cls,nb_working_file_path:str):
         # 初期セットアップ完了ステータスファイルを作成する
         cs = ContainerSetter(nb_working_file_path)
-        setup_flag_file = Path(cs.setup_completed_file_path)
-        setup_flag_file.touch()
+
+        # 初期セットアップ完了ステータスファイルの存在をチェックする
+        if os.path.exists(cs.setup_completed_file_path):
+            # 存在する場合
+            # 初期セットアップ済みとして、エラーにする
+            alert = pn.pane.Alert(msg_config.get('setup', 'setup_completed'),sizing_mode="stretch_width",alert_type='warning')
+            display(alert)
+            cs.display_main_menu(nb_working_file_path)
+            raise Exception('Cannot run because initial setup is complete')
+        else:
+            # 存在しない場合
+            # 初期セットアップ完了ステータスファイルを作成する
+            setup_flag_file = Path(cs.setup_completed_file_path)
+            setup_flag_file.touch()
+
 
     @classmethod
     def return_main_menu(cls,nb_working_file_path:str):
         pn.extension()
-        access_main_menu(nb_working_file_path)
+        cs = ContainerSetter(nb_working_file_path)
+        cs.display_main_menu(cs.nb_working_file_path)

ef4b748decb0b0f107d6bd3e5bf397ffc05dccfc
Kaho Kondo <kaho.kondo@ivis.co.jp>
2023-11-14 15:53:58 +0900

    update message

--- data_gorvernance/library/utils/save.py
+++ data_gorvernance/library/utils/save.py
@@ -62,12 +62,12 @@ class TaskSave(TaskLog):
             self.project_id = project_id
             self._save()
         else:
-            # grdmから起動しない場合（開発用？）
+            # grdmから起動しない場合（開発用）
             self._project_id_form()
 
     def _validate_token(self, token):
         if len(token) <= 0:
-            message = "入力されていません。入力してから再度クリックしてください。"
+            message = msg_config.get('save', 'empty_warning')
             self.log.warning(message)
             self._save_submit_button.set_looks_warning(message)
             return False
@@ -77,17 +77,17 @@ class TaskSave(TaskLog):
                 grdm.SCHEME, grdm.DOMAIN, token
             )
         except UnauthorizedError:
-            message = "正しいトークンを入力してください。"
+            message = msg_config.get('save', 'token_unauthorized')
             self.log.warning(message)
             self._save_submit_button.set_looks_warning(message)
             return False
         except RequestException:
-            message = "通信不良"
+            message = msg_config.get('save', 'connection_error')
             self.log.error(message)
             self._save_submit_button.set_looks_error(message)
             return False
         except Exception:
-            message = "想定外のエラー"
+            message = msg_config.get('DEFAULT', 'unexpected_error')
             self.log.error(message)
             self._save_submit_button.set_looks_error(message)
             return False
@@ -116,7 +116,6 @@ class TaskSave(TaskLog):
         self._save_submit_button.on_click(self._id_form_callback)
         self.save_form_box.append(self._save_submit_button)
 
-    @TaskLog.callback_form("select_grdm_project")
     def _project_select_callback(self, event):
         # NOTE: 一度値を格納してからでないと上手く動かない
         value = self._save_form.value
@@ -125,9 +124,12 @@ class TaskSave(TaskLog):
         else:
             self._save_submit_button.disabled = True
 
+    @TaskLog.callback_form("select_grdm_project")
     def _id_form_callback(self, event):
         if not self._save_form.value:
-            self._save_submit_button.set_looks_warning("値を選択してください。")
+            message = msg_config.get('form', 'select_error')
+            self._save_submit_button.set_looks_warning("message")
+            self.log.warning(message)
         self.project_id = self._save_form.value
         self._save()
 
@@ -137,7 +139,7 @@ class TaskSave(TaskLog):
 
         # start
         self.save_form_box.clear()
-        msg = '同期中です。しばらくお待ちください。'
+        msg = msg_config.get('form', 'doing')
         timediff.start()
 
         try:
@@ -150,6 +152,14 @@ class TaskSave(TaskLog):
                     abs_source = path,
                     abs_root=self._abs_root_path
                 )
+        except RequestException as e:
+            timediff.end()
+            minutes, seconds = timediff.get_diff_minute()
+            error_summary = traceback.format_exception_only(type(e), e)[0].rstrip('\\n')
+            error_msg = msg_config.get('save', 'connection_error') + "\n" + error_summary
+            self.log.error(error_msg)
+            self.save_msg_output.add_error(f'経過時間: {minutes}m {seconds}s\n {error_msg}')
+            return False
         except Exception as e:
             timediff.end()
             minutes, seconds = timediff.get_diff_minute()
@@ -161,4 +171,5 @@ class TaskSave(TaskLog):
         # end
         timediff.end()
         minutes, seconds = timediff.get_diff_minute()
-        self.save_msg_output.update_success(f'GRDMへの同期が完了しました。（{minutes}m {seconds}s）')
+        message = msg_config.get('save', 'success')
+        self.save_msg_output.update_success(f'{message}（{minutes}m {seconds}s）')

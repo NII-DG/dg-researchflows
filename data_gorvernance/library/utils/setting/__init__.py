889e69721fa1f60bbebaa88ed010a942db1a3e4c
Kaho Kondo <kaho.kondo@ivis.co.jp>
2023-12-04 09:49:28 +0900

    update file name

--- data_gorvernance/library/utils/setting/__init__.py
+++ data_gorvernance/library/utils/setting/__init__.py
@@ -1,4 +1,4 @@
 from .research_flow_status import ResearchFlowStatusOperater, get_subflow_type_and_id
-from .field_config import Field
+from .field import Field
 from .dg_customize_config import get_dg_customize_config, SubFlowRule, AlphaProperty
 from .status import SubflowTask, SubflowStatus, SubflowStatusFile

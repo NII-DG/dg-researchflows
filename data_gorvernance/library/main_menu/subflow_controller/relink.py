b1d1873e4f74fe11715832178f5147fdfb122e11
Kaho Kondo <kaho.kondo@ivis.co.jp>
2024-01-11 17:58:55 +0900

    modify sub_flow_type_options

--- data_gorvernance/library/main_menu/subflow_controller/relink.py
+++ data_gorvernance/library/main_menu/subflow_controller/relink.py
@@ -34,7 +34,7 @@ class RelinkSubflowForm(BaseSubflowForm):
             # experimentはplanのみを親とするため
             if phase_status._seq_number == 2:
                 continue
-            else:
+            if len(phase_status._sub_flow_data) > 1:
                 pahse_options[msg_config.get('research_flow_phase_display_name',phase_status._name)] = phase_status._seq_number
         return pahse_options
 

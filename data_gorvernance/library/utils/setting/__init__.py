db4632540f6f69325086b1512602edf95f152c07
ivis-akaike <nozomu.akaike@ivis.co.jp>
2024-01-24 10:05:28 +0900

    修正漏れを対応

--- data_gorvernance/library/utils/setting/__init__.py
+++ data_gorvernance/library/utils/setting/__init__.py
@@ -1,6 +1,6 @@
 from .research_flow_status import ResearchFlowStatusOperater, get_subflow_type_and_id
 from .field import Field
-from .ocs_template import ocs_template
+from .ocs_template import OCSTemplate
 from .dg_customize_config import get_dg_customize_config, SubFlowRule, AlphaProperty
 from .status import SubflowTask, SubflowStatus, SubflowStatusFile
 from .dmp import DMPManager

b921c3cb9d5bc251754072457ee502aa8cd69417
Kaho Kondo <kaho.kondo@ivis.co.jp>
2024-01-30 14:09:05 +0900

    modify handling token

--- data_gorvernance/library/utils/token.py
+++ data_gorvernance/library/utils/token.py
@@ -1,7 +1,9 @@
 
+from IPython.display import clear_output
 
 from .config import path_config, message as msg_config
 from .storage_provider import grdm
+from .checker import StringManager
 
 
 
@@ -12,8 +14,10 @@ def get_token():
         return token
     while True:
         token = input(msg_config.get('form', 'pls_input_token'))
-        if token:
+        token = StringManager.strip(token)
+        if not StringManager.is_empty(token):
             break
+    clear_output()
     return token
 
 def get_project_id():
@@ -22,6 +26,8 @@ def get_project_id():
         return project_id
     while True:
         project_id = input(msg_config.get('form', 'pls_input_project_id'))
-        if project_id:
+        project_id = StringManager.strip(project_id)
+        if not StringManager.is_empty(project_id):
             break
+    clear_output()
     return project_id

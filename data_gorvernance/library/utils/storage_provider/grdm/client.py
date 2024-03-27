1a36d977b61b1a0f6d89810e105e6722e3eebbca
Kaho Kondo <kaho.kondo@ivis.co.jp>
2024-03-27 14:26:01 +0900

    change storege

--- data_gorvernance/library/utils/storage_provider/grdm/client.py
+++ data_gorvernance/library/utils/storage_provider/grdm/client.py
@@ -1,14 +1,150 @@
 import os
+import six
+from requests.exceptions import ConnectionError
 
 from osfclient.cli import OSF, split_storage
+from osfclient.models.core import OSFCore
+from osfclient.models.file import ContainerMixin
+from osfclient.models.file import File
+from osfclient.utils import checksum
+from osfclient.utils import file_empty
+from osfclient.utils import get_local_file_size
+from osfclient.utils import norm_remote_path
 
-from ...file import File
+from ...file import File as ff
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 logfilepath = os.path.abspath(os.path.join(script_dir, "sync_log"))
-logfile = File(logfilepath)
+logfile = ff(logfilepath)
 if not os.path.exists(logfilepath):
     logfile.create()
+
+
+class Storage(OSFCore, ContainerMixin):
+    _files_key = ('relationships', 'files', 'links', 'related', 'href')
+
+    def _update_attributes(self, storage):
+        if not storage:
+            return
+
+        self.id = self._get_attribute(storage, 'id')
+
+        self.path = self._get_attribute(storage, 'attributes', 'path')
+        self.name = self._get_attribute(storage, 'attributes', 'name')
+        self.materialized_path = self._get_attribute(storage, 'attributes', 'materialized_path')
+        self.node = self._get_attribute(storage, 'attributes', 'node')
+        self.provider = self._get_attribute(storage, 'attributes', 'provider')
+
+        self._files_url = self._get_attribute(storage, *self._files_key)
+
+        self._new_folder_url = self._get_attribute(storage,
+                                                   'links', 'new_folder')
+        self._new_file_url = self._get_attribute(storage, 'links', 'upload')
+
+    def __str__(self):
+        return '<Storage [{0}]>'.format(self.id)
+
+    @property
+    def files(self):
+        """Iterate over all files in this storage.
+
+        Recursively lists all files in all subfolders.
+        """
+        return self._iter_children(self._files_url, 'file', File,
+                                   self._files_key)
+
+    def create_file(self, path, fp, force=False, update=False):
+        """Store a new file at `path` in this storage.
+
+        The contents of the file descriptor `fp` (opened in 'rb' mode)
+        will be uploaded to `path` which is the full path at
+        which to store the file.
+
+        To force overwrite of an existing file, set `force=True`.
+        To overwrite an existing file only if the files differ, set `update=True`
+        """
+        if 'b' not in fp.mode:
+            raise ValueError("File has to be opened in binary mode.")
+
+        # all paths are assumed to be absolute
+        path = norm_remote_path(path)
+
+        directory, fname = os.path.split(path)
+        directories = directory.split(os.path.sep)
+        # navigate to the right parent object for our file
+        parent = self
+        contents = logfile.read()
+        contents += f'\nparent.materialized_path:{parent.materialized_path}'
+        logfile.write(contents)
+        for directory in directories:
+            # skip empty directory names
+            contents = logfile.read()
+            contents += f'\ndirectory:{directory}'
+            logfile.write(contents)
+            if directory:
+                parent = parent.create_folder(directory, exist_ok=True)
+                contents = logfile.read()
+                contents += f'\nparent.materialized_path:{parent.materialized_path}'
+                logfile.write(contents)
+
+        url = parent._new_file_url
+        contents = logfile.read()
+        contents += f'\nparent._new_file_url:{parent._new_file_url}'
+        logfile.write(contents)
+
+        # When uploading a large file (>a few MB) that already exists
+        # we sometimes get a ConnectionError instead of a status == 409.
+        connection_error = False
+
+        # peek at the file to check if it is an empty file which needs special
+        # handling in requests. If we pass a file like object to data that
+        # turns out to be of length zero then no file is created on the OSF.
+        # See: https://github.com/osfclient/osfclient/pull/135
+        if file_empty(fp):
+            response = self._put(url, params={'name': fname}, data=b'')
+        else:
+            try:
+                response = self._put(url, params={'name': fname}, data=fp)
+            except ConnectionError:
+                connection_error = True
+
+        if connection_error or response.status_code == 409:
+            if not force and not update:
+                # one-liner to get file size from file pointer from
+                # https://stackoverflow.com/a/283719/2680824
+                file_size_bytes = get_local_file_size(fp)
+                large_file_cutoff = 2**20 # 1 MB in bytes
+                if connection_error and file_size_bytes < large_file_cutoff:
+                    msg = (
+                        "There was a connection error which might mean {} " +
+                        "already exists. Try again with the `--force` flag " +
+                        "specified."
+                    ).format(path)
+                    raise RuntimeError(msg)
+                else:
+                    # note in case of connection error, we are making an inference here
+                    raise FileExistsError(path)
+
+            else:
+                # find the upload URL for the file we are trying to update
+                for file_ in self.files:
+                    if norm_remote_path(file_.path) == path:
+                        if not force:
+                            if checksum(path) == file_.hashes.get('md5'):
+                                # If the hashes are equal and force is False,
+                                # we're done here
+                                break
+                        # in the process of attempting to upload the file we
+                        # moved through it -> reset read position to beginning
+                        # of the file
+                        fp.seek(0)
+                        file_.update(fp)
+                        break
+                else:
+                    raise RuntimeError("Could not create a new file at "
+                                       "({}) nor update it.".format(path))
+
+
 
 class UpdateArgs:
 
@@ -33,7 +169,20 @@ def upload(token, base_url, args):
     project = osf.project(args.project)
     storage, remote_path = split_storage(args.destination)
 
-    store = project.storage(storage)
+    #store = project.storage(storage)
+    stores = project._json(project._get(project._storages_url), 200)
+    stores = stores['data']
+    store = None
+    for sto in stores:
+        provides = project._get_attribute(sto, 'attributes', 'provider')
+        if provides == storage:
+            store =  Storage(sto, project.session)
+            break
+    else:
+        raise RuntimeError("Project has no storage "
+                           "provider '{}'".format(storage))
+
+
     if args.recursive:
         if not os.path.isdir(args.source):
             raise RuntimeError("Expected source ({}) to be a directory when "
@@ -51,21 +200,21 @@ def upload(token, base_url, args):
                     name = os.path.join(remote_path, dir_name, subdir_path,
                                         fname)
                     contents = logfile.read()
-                    contents += f'\nname:{name}\nbefore_new_file_url:{store._new_file_url}\nbefore_new_folder_url:{store._new_folder_url}'
+                    contents += f'\nname:{name}\nbefore_new_file_url:{store._new_file_url}\nbefore_new_folder_url:{store._new_folder_url}\nbefore_files_url:{store._files_url}'
                     logfile.write(contents)
                     store.create_file(name, fp, force=args.force,
                                         update=args.update)
                     contents = logfile.read()
-                    contents += f'\nafter_new_file_url:{store._new_file_url}\nafter_new_folder_url:{store._new_folder_url}'
+                    contents += f'\nafter_new_file_url:{store._new_file_url}\nafter_new_folder_url:{store._new_folder_url}\nafter_files_url:{store._files_url}'
                     logfile.write(contents)
 
     else:
         contents = logfile.read()
-        contents += f'\nname:{remote_path}\nbefore_new_file_url:{store._new_file_url}\nbefore_new_folder_url:{store._new_folder_url}'
+        contents += f'\nname:{remote_path}\nbefore_new_file_url:{store._new_file_url}\nbefore_new_folder_url:{store._new_folder_url}\nbefore_files_url:{store._files_url}'
         logfile.write(contents)
         with open(args.source, 'rb') as fp:
             store.create_file(remote_path, fp, force=args.force,
                                 update=args.update)
         contents = logfile.read()
-        contents += f'\nafter_new_file_url:{store._new_file_url}\nafter_new_folder_url:{store._new_folder_url}'
+        contents += f'\nafter_new_file_url:{store._new_file_url}\nafter_new_folder_url:{store._new_folder_url}\nbefore_files_url:{store._files_url}'
         logfile.write(contents)

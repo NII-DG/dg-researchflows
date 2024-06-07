31fd2b7c7ca3eaa6d2f9635be415bc61bf0edac3
Kaho Kondo <kaho.kondo@ivis.co.jp>
2024-06-07 14:06:17 +0900

    update domain for test

--- data_gorvernance/library/utils/storage_provider/grdm/__init__.py
+++ data_gorvernance/library/utils/storage_provider/grdm/__init__.py
@@ -4,7 +4,7 @@ from .api import *
 
 SCHEME = 'https'
 
-DOMAIN = 'rdm.nii.ac.jp'
+DOMAIN = 'rcos.rdm.nii.ac.jp'
 
-API_V2_BASE_URL = 'https://api.rdm.nii.ac.jp/v2/'
-API_DOMAIN = 'api.rdm.nii.ac.jp'
+API_V2_BASE_URL = 'https://api.rcos.rdm.nii.ac.jp/v2/'
+API_DOMAIN = 'api.rcos.rdm.nii.ac.jp'

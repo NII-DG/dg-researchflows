624bf55c6119b58b63b3f308bea70054c5b5391d
Kaho Kondo <kaho.kondo@ivis.co.jp>
2024-06-17 14:09:06 +0900

    Extract common logic into a new method

--- data_gorvernance/library/utils/input.py
+++ data_gorvernance/library/utils/input.py
@@ -1,62 +1,12 @@
 import getpass
+from typing import Callable
 
 from . import dg_web
 from .config import message as msg_config
 from .storage_provider import grdm
 from .string import StringManager
 from .vault import Vault
-from .error import UnauthorizedError, UnusableVault
-
-
-def get_grdm_token():
-    """トークンを取得する
-
-    Raises:
-        UnusableVault: vaultが利用できない
-        requests.exceptions.RequestException: 通信不良
-
-    Returns:
-        str: トークン
-    """
-
-    TOKEN_KEY = 'grdm_token'
-
-    # Vaultからトークンを取得する
-    try:
-        vault = Vault()
-        token = vault.get_value(TOKEN_KEY)
-    except Exception as e:
-        raise UnusableVault from e
-
-    if token:
-        # 接続確認
-        try:
-            grdm.get_projects(grdm.SCHEME, grdm.API_DOMAIN, token)
-        except UnauthorizedError:
-            pass
-        else:
-            return token
-
-    while True:
-        token = getpass.getpass(msg_config.get('form', 'pls_input_grdm_token'))
-
-        # 形式確認
-        token = StringManager.strip(token)
-        if StringManager.is_empty(token):
-            continue
-        if not StringManager.is_half(token):
-            continue
-
-        # 接続確認
-        try:
-            grdm.get_projects(grdm.SCHEME, grdm.API_DOMAIN, token)
-        except UnauthorizedError:
-            continue
-
-        break
-
-    vault.set_value(TOKEN_KEY, token)
-    return token
+from .error import UnusableVault
 
 
 def get_project_id():
@@ -75,33 +25,32 @@ def get_project_id():
     return project_id
 
 
-def get_goveredrun_token():
-    """トークンを取得する
+def get_token(key:str, func:Callable[[str], bool], message:str):
+    """vaultもしくはinputからトークンを取得する
+
+    Args:
+        key (str): トークンをvaultで保存するときのキー
+        func (Callable[[str], bool]): トークンの有効性を確認するメソッド
+        message(str): inputを求める際の表示メッセージ
 
     Raises:
-        UnusableVault: vaultが利用できない
-        requests.exceptions.RequestException: 通信不良
+        UnusableVault: vaultの利用に不具合があったときのエラー
 
     Returns:
         str: トークン
     """
-
-    TOKEN_KEY = 'govrun_token'
-
-    # Vaultからトークンを取得する
     try:
         vault = Vault()
-        token = vault.get_value(TOKEN_KEY)
+        token = vault.get_value(key)
     except Exception as e:
         raise UnusableVault from e
 
-    if token:
-        # 有効性確認
-        if dg_web.check_governedrun_token(grdm.SCHEME, grdm.API_DOMAIN, token):
-            return token
+    # 有効性確認
+    if token and func(token):
+        return token
 
     while True:
-        token = getpass.getpass(msg_config.get('form', 'pls_input_govrun_token'))
+        token = getpass.getpass(message)
 
         # 形式確認
         token = StringManager.strip(token)
@@ -110,11 +59,48 @@ def get_goveredrun_token():
         if not StringManager.is_half(token):
             continue
 
-        # 接続確認
-        if dg_web.check_governedrun_token(grdm.SCHEME, grdm.API_DOMAIN, token):
-            return token
+        # 有効性確認
+        if not func(token):
+            continue
 
         break
 
-    vault.set_value(TOKEN_KEY, token)
+    vault.set_value(key, token)
     return token
+
+
+def get_grdm_token():
+    """GRDMのパーソナルアクセストークンを取得する
+
+    Raises:
+        UnusableVault: vaultが利用できない
+        requests.exceptions.RequestException: 通信不良
+
+    Returns:
+        str: パーソナルアクセストークン
+    """
+    def check_auth(token):
+        try:
+            grdm.get_projects(grdm.SCHEME, grdm.API_DOMAIN, token)
+        except Exception:
+            return False
+        else:
+            return True
+
+    return get_token('grdm_token', check_auth, msg_config.get('form', 'pls_input_grdm_token'))
+
+
+def get_goveredrun_token():
+    """Governed Runのトークンを取得する
+
+    Raises:
+        UnusableVault: vaultが利用できない
+        requests.exceptions.RequestException: 通信不良
+
+    Returns:
+        str: Governed Runのトークン
+    """
+    def check_auth(token):
+        return dg_web.check_governedrun_token(grdm.SCHEME, grdm.API_DOMAIN, token)
+
+    return get_token('govrun_token', check_auth, msg_config.get('form', 'pls_input_govrun_token'))

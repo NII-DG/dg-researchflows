d4e3652abf6ff0da6e767b07e3b4001eb2835456
Kihiro.Ashino <kihiro.ashino@ivis.co.jp>
2024-08-09 16:27:37 +0900

    use singleton pattern

--- data_gorvernance/library/utils/config/message.py
+++ data_gorvernance/library/utils/config/message.py
@@ -6,6 +6,35 @@ MESSAGE_CONFIG_PATH = '../../data/message.ini'
 script_dir = os.path.dirname(os.path.abspath(__file__))
 message_ini_path = os.path.abspath(os.path.join(script_dir, MESSAGE_CONFIG_PATH))
 
+class Message(configparser.ConfigParser):
+    """ メッセージファイルを保持するためのシングルトンなクラスです。
+
+    Args:
+        class:
+            _instance (Message): シングルトンインスタンスを格納する
+
+    """
+
+    def __new__(cls, *args, **kwargs)->'Message':
+        """ 新しいインスタンス作成時に既存のインスタンスを返すメソッドです。
+
+        Returns:
+            Message: シングルトンインスタンスを返す。
+
+        """
+        if not hasattr(cls, "_instance"):
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, *args, **kwargs):
+        """ インスタンスの初期化処理を実行するメソッドです。
+
+        インスタンス初期化時に設定ファイルを読み込みます。
+
+        """
+        super().__init__(*args, **kwargs)
+        self.read(message_ini_path, encoding='utf-8')
+
 
 def get(section:str, option:str) -> str:
     """ メッセージを取得する関数です。
@@ -17,7 +46,5 @@ def get(section:str, option:str) -> str:
         str: メッセージを返す。
 
     """
-    config = configparser.ConfigParser()
-    config.read(message_ini_path, encoding='utf-8')
-
+    config = Message()
     return config[section][option]

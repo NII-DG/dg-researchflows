bb0de561b64c00bcdad2bb4058f0b33408e2f345
ivis-ikeda <yuta.ikeda@ivis.co.jp>
2024-08-22 15:41:03 +0900

    マージ競合部分の修正２

--- data_gorvernance/library/subflow/subflow.py
+++ data_gorvernance/library/subflow/subflow.py
@@ -94,11 +94,7 @@ class SubFlowManager:
                         target_is_directory=True
                     )
 
-<<<<<<< HEAD
-    def generate(self, svg_path: str, tmp_diag: str, font: str, display_all: bool = True) -> None:
-=======
     def generate(self, svg_path: str, tmp_diag: str, font: str) -> None:
->>>>>>> 02fbe48a00ce6da0d0f0fcbb92f3b4698ead1844
         """ ダイアグラムを生成するメソッドです。
 
         Args:
@@ -120,45 +116,12 @@ class SubFlowManager:
         self.diag.generate_svg(tmp_diag, svg_path, font)
         update_svg(svg_path, self.current_dir, self.svg_config)
 
-<<<<<<< HEAD
-    def _update(self, display_all: bool = True) -> None:
-        """ タスクの状態に基づいてダイアグラムを更新するメソッドです。
-
-        Args:
-            display_all (bool): 非推奨のタスクを推奨タスクと同じように表示するかどうかを設定します。
-
-        """
-        for task in self.tasks:
-            self._adjust_by_status(task, display_all)
-            self._adjust_by_optional(task, display_all)
-
-    def _adjust_by_optional(self, task: SubflowTask, display_all: bool = True) -> None:
-        """ 非推奨のタスクをグレーアウトし、リンクが押せない状態にするメソッドです。
-
-        Args:
-            task (SubflowTask): 調整するタスクを設定します。
-            display_all (bool): 非推奨のタスクを通常通りに表示するかどうかを設定します。
-
-        """
-        if task.disable:
-            if display_all:
-                # self.diag.update_node_style(task.id, 'dotted')
-                pass
-            else:
-                # self.diag.delete_node(task.id)
-                # 以下暫定処理
-                self.diag.update_node_color(task.id, "#77787B")
-                self.svg_config[task.id]['is_link'] = False
-
-    def _adjust_by_status(self, task: SubflowTask, display_all: bool = True) -> None:
-=======
     def _update(self) -> None:
         """ タスクの状態に基づいてダイアグラムを更新するメソッドです。"""
         for task in self.tasks:
             self._adjust_by_status(task)
             
     def _adjust_by_status(self, task: SubflowTask) -> None:
->>>>>>> 02fbe48a00ce6da0d0f0fcbb92f3b4698ead1844
         """ フロー図の見た目をタスクの状態によって変えるメソッドです。
 
         Args:

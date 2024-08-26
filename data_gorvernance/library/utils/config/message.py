7d13cb44e940672d491137841e7cd548c317cdc2
ivis-ikeda <yuta.ikeda@ivis.co.jp>
2024-08-26 11:54:07 +0900

    develop側に合わせて修正

--- data_gorvernance/library/utils/config/message.py
+++ data_gorvernance/library/utils/config/message.py
@@ -1,10 +1,14 @@
-""" メッセージ取得のモジュールです。"""
-from mailbox import Message
+""" 設定ファイルからメッセージを取得するためのモジュールです。"""
 import os
+
+from .config_parser_base import ConfigParserBase
 
 MESSAGE_CONFIG_PATH = '../../data/message.ini'
 script_dir = os.path.dirname(os.path.abspath(__file__))
 message_ini_path = os.path.abspath(os.path.join(script_dir, MESSAGE_CONFIG_PATH))
+
+class Message(ConfigParserBase):
+    """ message.iniを読み込むためのクラスです。"""
 
 
 def get(section: str, option: str) -> str:
@@ -15,7 +19,6 @@ def get(section: str, option: str) -> str:
         option (str): message.iniのキーを設定します。
     Returns:
         str: メッセージを返す。
-
     """
     config = Message(message_ini_path)
-    return config[section][option]
+    return config.get(section, option)

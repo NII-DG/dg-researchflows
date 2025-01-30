e3e7615b9b954c3361330ae311f424a35bd5f0b6
Yuta.Ikeda <yuta.ikeda@ivis.co.jp>
2025-01-30 17:07:27 +0900

    最終確認

--- data_governance/library/utils/pdf_metadata.py
+++ data_governance/library/utils/pdf_metadata.py
@@ -19,36 +19,54 @@ class PdfMetaData(TaskDirector):
     """PDFのメタデータを扱うクラスです。"""
 
     def __init__(self, working_path: str, notebook_name: str):
+        """PdfMetaDataクラスのコンストラクタです。
+
+        Args:
+            working_path (str): 実行Notebookファイルパス
+            notebook_name (str): ノートブック名
+
+        """
         super().__init__(working_path, notebook_name)
 
+        #入力フォーム
         pn.extension()
         self.author_input = pn.widgets.TextInput(name='Author', value='')
         self.title_input = pn.widgets.TextInput(name='Title', value='')
         self.subject_input = pn.widgets.TextInput(name='Subject', value='')
+        self.keyword_input = pn.widgets.TextInput(name='keyword', value='')
 
+        # メッセージボックス
         self.output_message = MessageBox()
         self.output_message.width = 900
 
+        # 表示用のフォーム
         self.form_section = pn.WidgetBox()
 
     def generate_pdf_metadata_form(self, dir_path: str):
-        """pdfを選択してメタデータを登録するためのフォームを生成します。"""
+        """pdfを選択してメタデータを登録するためのフォームを生成するメソッドです。
 
+        Args:
+            dir_path (str): 対象のpdfが保存されているディレクトリのパス
+
+        """
         self.doing_task()
 
-        form = pn.Column(self.author_input, self.title_input, self.subject_input)
+        form = pn.Column(self.author_input, self.title_input, self.subject_input, self.keyword_input)
 
+        # PDFのセレクトボックスの設定
         pdf_files = self.get_pdf_files(dir_path)
         pdf_files[msg_config.get('register_paper_metadata', 'form_title')] = 'default'  # Default
         self.pdf_select = pn.widgets.Select(name=msg_config.get('register_paper_metadata', 'form_name'), options=pdf_files, value="default")
         self.pdf_select.param.watch(self.on_select_change, 'value')
 
+        # 登録ボタンの設定
         registrate_button_title = msg_config.get('form', 'registrate')
         self.registrate_button = Button(width=500, disabled=True)
         self.registrate_button.set_looks_init(registrate_button_title)
         self.registrate_button.on_click(self.on_click_registrate)
 
         self.done_task()
+        # 表示
         self.form_section.append(self.pdf_select)
         self.form_section.append(form)
         self.form_section.append(self.registrate_button)
@@ -56,12 +74,21 @@ class PdfMetaData(TaskDirector):
         display(Javascript('IPython.notebook.save_checkpoint();'))
 
     def get_pdf_files(self, dir_path: str) -> dict:
-        """指定されたディレクトリ内のpdfファイルを取得するメソッドです。"""
+        """指定されたディレクトリ内のpdfファイルを取得するメソッドです。
+
+        Args:
+            dir_path (str): pdfが保存されているディレクトリのパス
+
+        Returns:
+            dict: キーをファイル名、値をフルパスにしたpdfファイルの情報
+
+        """
         pdf_files = [f for f in os.listdir(dir_path) if f.endswith('.pdf')]
         return {file: os.path.join(dir_path, file) for file in pdf_files}
 
     def on_select_change(self, event):
         """PDFが選択された際にメタデータを取得するメソッドです。"""
+        # pdfが選択されたときにメタデータを取得して入力欄に入れる
         pdf_path = self.pdf_select.value
         if pdf_path != 'default':
             with pikepdf.open(pdf_path) as pdf:
@@ -69,10 +96,12 @@ class PdfMetaData(TaskDirector):
                 author = metadata.get('/Author', '')
                 title = metadata.get('/Title', '')
                 subject = metadata.get('/Subject', '')
+                keywords = metadata.get('/Keywords', '')
 
             self.author_input.value = author
             self.title_input.value = title
             self.subject_input.value = subject
+            self.keyword_input.value = keywords
 
             self.registrate_button.disabled = False
         else:
@@ -80,22 +109,26 @@ class PdfMetaData(TaskDirector):
             self.author_input.value = ""
             self.title_input.value = ""
             self.subject_input.value = ""
+            self.keyword_input.value = ""
             self.registrate_button.disabled = True
 
+    @TaskDirector.callback_form('PDFのメタデータを登録する')
     def on_click_registrate(self, event):
         """メタデータを登録するメソッドです。"""
         author = self.author_input.value
         title = self.title_input.value
         subject = self.subject_input.value
+        keywords = self.keyword_input.value
 
         pdf_path = self.pdf_select.value
         if pdf_path != 'default':
             with pikepdf.open(pdf_path, allow_overwriting_input=True) as pdf:
-                # メタデータを直接更新
+                # メタデータを更新
                 self.log.info(f"Updating metadata for {pdf_path}")
                 pdf.docinfo['/Author'] = author
                 pdf.docinfo['/Title'] = title
                 pdf.docinfo['/Subject'] = subject
+                pdf.docinfo['/Keywords'] = keywords
 
                 # PDFを保存（元のファイルに上書き）
                 pdf.save(pdf_path)

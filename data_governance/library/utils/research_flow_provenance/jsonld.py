03527b4971a485c309a27d6d270cd1ab57554b9a
Kaho Kondo <kaho.kondo@ivis.co.jp>
2025-09-29 12:02:35 +0900

    2025-1Q-1-IT-021, 2025-1Q-1-IT-027

--- data_governance/library/utils/research_flow_provenance/jsonld.py
+++ data_governance/library/utils/research_flow_provenance/jsonld.py
@@ -453,3 +453,11 @@ class ProvenanceEditor:
                 json.dump(entity_data, f, indent=2, ensure_ascii=False)
         except IOError as e:
             raise RuntimeError(f"{self.activity_file}の書き込みに失敗しました: {e}") from e
+
+    def get_file_list(self) -> list:
+        """来歴ファイルのリストを返すメソッドです。
+
+        Returns:
+            list: 来歴ファイルのリスト
+        """
+        return [self.entity_file, self.activity_file, self.agent_file]
